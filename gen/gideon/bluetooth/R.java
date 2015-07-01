430f47cdecd9d21329d2ed06c2b6e7bffd5fb6c3
janpansa <appiusa@gmail.com>
2015-07-01 21:04:16 +0200

    Added a selector for the navbar buttons if we want other colors later... just made it 2a2a2a for now.

--- gen/gideon/bluetooth/R.java
+++ gen/gideon/bluetooth/R.java
@@ -15,13 +15,15 @@ public final class R {
     }
     public static final class color {
         public static final int dark=0x7f060000;
+        public static final int light=0x7f060001;
     }
     public static final class drawable {
         public static final int action_search=0x7f020000;
-        public static final int drawer_shadow=0x7f020001;
-        public static final int ic_drawer=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int ic_launcher_1=0x7f020004;
+        public static final int activated_background=0x7f020001;
+        public static final int drawer_shadow=0x7f020002;
+        public static final int ic_drawer=0x7f020003;
+        public static final int ic_launcher=0x7f020004;
+        public static final int ic_launcher_1=0x7f020005;
     }
     public static final class id {
         public static final int content_frame=0x7f090004;
