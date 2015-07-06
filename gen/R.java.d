8551b7ebfa1048b48ccc9f7d5e680a742ac4cf89
janpansa <appiusa@gmail.com>
2015-07-06 21:57:01 +0200

    Committing after building the app...

--- gen/R.java.d
+++ gen/R.java.d
@@ -24,4 +24,14 @@
 /home/gideon/NetBeansProjects/arduino_bluetooth_android/res/layout/screen_help.xml \
 /home/gideon/NetBeansProjects/arduino_bluetooth_android/res/layout/menu_list_device.xml \
 /home/gideon/NetBeansProjects/arduino_bluetooth_android/res/drawable-mdpi/ic_launcher.png \
+/home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/res/drawable-hdpi/ic_launcher.png \
+/home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/res/drawable-xhdpi/ic_launcher.png \
+/home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/res/drawable-xhdpi/ic_launcher_1.png \
+/home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/res/drawable-xhdpi/action_search.png \
+/home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/res/drawable-xhdpi/drawer_shadow.9.png \
+/home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/res/drawable-xhdpi/ic_drawer.png \
+/home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/res/drawable-ldpi/ic_launcher.png \
+/home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/res/drawable/bluetooth.png \
+/home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/res/drawable-xxhdpi/ic_launcher.png \
+/home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/res/drawable-mdpi/ic_launcher.png \
 /home/gideon/NetBeansProjects/arduino_bluetooth_android/bin/AndroidManifest.xml \
