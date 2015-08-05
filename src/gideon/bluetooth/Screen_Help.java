1e107390529c91d26a4ec294df8b2297c748ace7
janpansa <appiusa@gmail.com>
2015-08-05 21:46:49 +0200

    Created Screen_Help and moved it's code. Now for the test :-) ...

---
+++ src/gideon/bluetooth/Screen_Help.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gideon.bluetooth;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ *
+ * @author gideon
+ */
+public class Screen_Help extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
+    {
+        //Verander die Title van die Fragment/Activity
+        getActivity().setTitle(getResources().getStringArray(R.array.menuitems)[3]);
+        
+        //Inflate die fragment met sy layout.
+        View rootView = inflater.inflate(R.layout.screen_help, container, false);
+        
+        //Doen veranderinge aan layout hier... bv Textviews ens...
+        return rootView;
+    }
+}
