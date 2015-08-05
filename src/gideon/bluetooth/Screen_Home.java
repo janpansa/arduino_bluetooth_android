3a20ce961279196130a8caa3edbf94b7d1d2a87c
janpansa <appiusa@gmail.com>
2015-08-05 21:28:15 +0200

    Created a Screen_Home fragment and moved home screen code into that fragment. Home in this case refers to the first fargment of the drawer, not the first activity the app loads... just to be clear :-)...

---
+++ src/gideon/bluetooth/Screen_Home.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gideon.bluetooth;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ *
+ * @author gideon
+ */
+public class Screen_Home extends Fragment
+{
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState)
+    {
+        //Verander die Title van die Fragment/Activity
+        getActivity().setTitle(getResources().getStringArray(R.array.menuitems)[0]);
+        
+        //Inflate die fragment met sy layout.
+        View rootView = inflater.inflate(R.layout.screen_settings, container, false);
+        
+        //Doen veranderinge aan layout hier... bv Textviews ens...
+        return rootView;
+    }
+}
